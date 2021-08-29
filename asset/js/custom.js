e6e69faf61686c54fcdd4fad6ccee4ea9d2b509c
Ojvar <MOjvar.AmirHossein@Gmail.com>
2021-08-29 18:58:13 +0430

    Fix index.html and custom.js

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,19 +1,62 @@
-var myApplication = new Vue({
-    el: '#app',
+new Vue({
+    el: "#app",
+
     data: {
-        counter: 0
-    
+        step: 3,
+        counter: 0,
+        x: 0,
+        y: 0,
     },
-    methods: {
-        increase: function(step){
-            this.counter+=step;
+
+    computed: {
+        state() {
+            return this.counter % 2 == 0 ? "Even" : "Odd";
         },
-        getCursor: function(event){
+        color() {
+            return this.counter % 2 == 0 ? "Green" : "Red";
+        },
+    },
+
+    methods: {
+        /**
+         * Increament counter by the specified value x 2
+         * @param {number} step The step value
+         */
+        inc2: function (step) {
+            this.counter += step * 2;
+        },
+
+        /**
+         * Decreament counter by the specified value x 2
+         * @param {number} step The step value
+         */
+        dec2: function (step) {
+            this.counter -= step * 2;
+        },
+
+        /**
+         * Increament counter by the specified value
+         * @param {number} step The step value
+         */
+        increase: function (step) {
+            this.counter += step;
+        },
+
+        /**
+         * Decreament counter by the specified value
+         * @param {number} step The step value
+         */
+        decrease: function (step) {
+            this.counter -= step;
+        },
+
+        /**
+         * Get cursor position
+         * @param {Event} event
+         */
+        getCursor: function (event) {
             this.x = event.clientX;
             this.y = event.clientY;
-
-        }
-
-    }
-
+        },
+    },
 });
